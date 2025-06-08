97d8fa562dfc2ecaf0ecbea35676a9b674ef185e
Navya Arora <navya0776arora@gmail.com>
2025-06-08 20:23:40 +0530

    yo

--- Website/Navya/skillMate/src/aboutme.js
+++ Website/Navya/skillMate/src/aboutme.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import photo from "./photo.jpg";
-import "./ProfileDetail.css";
+import "./profile.css";
 
 const profiles = [
   {
